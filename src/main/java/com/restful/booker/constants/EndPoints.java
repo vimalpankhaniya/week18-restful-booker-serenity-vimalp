8759a64ed8b278fede133c41e087be1f3c5b15c3
vimal pankhaniya <vimalpankhaniya2016@gmail.com>
2023-01-04 01:04:23 +0000

    week18-restful-booker-serenity-vimalp

---
+++ src/main/java/com/restful/booker/constants/EndPoints.java
@@ -0,0 +1,21 @@
+package com.restful.booker.constants;
+
+/**
+ * Created by Jay
+ */
+public class EndPoints {
+
+    /**
+     * This is Endpoints of go rest api
+     */
+    public static final String GET_SINGLE_BOOKING_BY_ID = "/{id}";
+    public static final String GET_All_BOOKING_BY_IDS = "/{id}";
+    public static final String UPDATE_BOOKING_BY_ID = "/{id}";
+    public static final String DELETE_BOOKING_BY_ID = "/{id}";
+
+
+    /**
+     * This is Endpoints of Authentication api
+     */
+        public static final String Authorization = "Bearer 600f4364266ef9256401822c412cbfa2a4fe3c13c5c708bf2206cbb120f2a4c9";
+}
